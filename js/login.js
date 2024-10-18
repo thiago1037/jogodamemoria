d3240ebed752ca0aaa5ba07a18e0cd731abd5940
Leandro Valk <106777426+leandropensamentocomputacional@users.noreply.github.com>
2024-10-18 16:16:42 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,14 +2,26 @@ const input = document.querySelector('.login__input');
 const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
 
-const validateInput = ({ target }) => {
+/*const validateInput = ({ target }) => {
   if (target.value.length > 3) {
     button.removeAttribute('disabled');
     return;
   }
 
   button.setAttribute('disabled', '');
+}*/
+
+const validateInput = ({ target }) => {  
+    if (target.value.length > 3 && target.value.length < 10) {    
+        button.removeAttribute('disabled');    
+        return;  
+    }  
+    button.setAttribute('disabled', ''); 
 }
+
+
+
+
 
 const handleSubmit = (event) => {
   event.preventDefault();
