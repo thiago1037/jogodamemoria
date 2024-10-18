55b689693349944bb6d0831fb52d236e61118cef
Leandro Valk <106777426+leandropensamentocomputacional@users.noreply.github.com>
2024-10-18 16:10:41 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,6 +6,9 @@ const validateInput = ({ target }) => {
   if (target.value.length > 3) {
     button.removeAttribute('disabled');
     return;
+  if (target.value.length < 10) {
+    button.removeAttribute('disabled');
+    return;
   }
 
   button.setAttribute('disabled', '');
@@ -20,3 +23,23 @@ const handleSubmit = (event) => {
 
 input.addEventListener('input', validateInput);
 form.addEventListener('submit', handleSubmit);
+
+/*function validarLogin(login) {
+    const minLength = 4;
+    const maxLength = 10;
+
+    if (login.length < minLength || login.length > maxLength) {
+        return false; // Login inválido
+    }
+    return true; // Login válido
+}
+
+// Exemplo de uso
+const login = "usuario123";
+if (validarLogin(login)) {
+    console.log("Login válido.");
+} else {
+    console.log("Login inválido. O login deve ter entre 4 e 10 letras.");
+}*/
+
+
